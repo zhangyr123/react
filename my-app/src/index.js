84b9504d2621f512efb2c10b5f67a51ac9daa72b
zhangyr <2195175822@qq.com>
2020-05-01 23:06:30 +0800

    官网学习create + jsx

---
+++ my-app/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName
+}
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>
+  }
+  return <h1>Hello, Stranger.</h1>
+}
+
+// const user = {
+//   firstName: 'Harper',
+//   lastName: 'Perez'
+// }
+
+const user = null
+
+const element = (
+  <h1>
+   {getGreeting(user)}
+  </h1>
+)
+
+ReactDOM.render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
+  element,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
