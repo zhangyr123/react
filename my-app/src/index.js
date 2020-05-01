cea726fcb60be1a77d2140d8e42d98626b9c83d9
zhangyr <2195175822@qq.com>
2020-05-02 04:41:46 +0800

    组件提取

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -94,6 +94,26 @@ import * as serviceWorker from './serviceWorker';
 // )
 
 // 提取组件
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  )
+}
+
+function UserInfo(props) {
+  return (
+    <div className="userInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  )
+}
+
 function formatDate(date) {
   return date.toLocaleDateString()
 }
@@ -101,20 +121,12 @@ function formatDate(date) {
 function Comment(props) {
   return (
     <div className="Comment">
-      <div className="UserInfo">
-        <img className="Avatar" 
-        src={props.author.avatarUrl}
-        alt={props.author.name}
-        />
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-        <div className="Comment-text">
-          {props.text}
-        </div>
-        <div className="Comment-date">
-          {formatDate(props.date)}
-        </div>
+      <UserInfo user={props.author} />
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
       </div>
     </div>
   )
