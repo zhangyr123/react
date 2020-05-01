da637ebc223d1b8adfbca7212109c0568d70c47e
zhangyr <2195175822@qq.com>
2020-05-02 04:28:16 +0800

    定义网站评论组件

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -39,17 +39,17 @@ import * as serviceWorker from './serviceWorker';
 // )
 
 // 计时器
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  )
-  ReactDOM.render(element, document.getElementById('root'))
-}
+// function tick() {
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//     </div>
+//   )
+//   ReactDOM.render(element, document.getElementById('root'))
+// }
 
-setInterval(tick, 1000)
+// setInterval(tick, 1000)
 
 // ReactDOM.render(
 //   // <React.StrictMode>
@@ -58,6 +58,83 @@ setInterval(tick, 1000)
 //   element,
 //   document.getElementById('root')
 // );
+
+// const props = {
+//   name: 'Bob'
+// }
+// function Welcome(props) {
+// return <h1>Hello, {props.name}</h1>
+// }
+// ReactDOM.render(
+//   Welcome(props),
+//   document.getElementById('root')
+// )
+
+
+// class Welcome extends React.Component {
+//   render() {
+//     return <h1>Hello, {this.props.name}</h1>
+//   }
+// }
+// 使用自定义组件
+// const element = <Welcome name="zhangyr"></Welcome>
+// 使用组合组件
+// function App() {
+//   return (
+//     <div>
+//       <Welcome name="Sara" />
+//       <Welcome name="Cahal" />
+//       <Welcome name="Edite" />
+//     </div>
+//   )
+// }
+// ReactDOM.render(
+//   <App/>,
+//   document.getElementById('root')
+// )
+
+// 提取组件
+function formatDate(date) {
+  return date.toLocaleDateString()
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img className="Avatar" 
+        src={props.author.avatarUrl}
+        alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+        <div className="Comment-text">
+          {props.text}
+        </div>
+        <div className="Comment-date">
+          {formatDate(props.date)}
+        </div>
+      </div>
+    </div>
+  )
+}
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
+ReactDOM.render(
+  <Comment 
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
+  document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
