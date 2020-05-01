80bf02c32afa620fc7e6e3baea7933fa2e8440e3
zhangyr <2195175822@qq.com>
2020-05-01 23:41:09 +0800

    JSX学习

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,29 +4,39 @@ import './index.css';
 // import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName
-}
+// function formatName(user) {
+//   return user.firstName + ' ' + user.lastName
+// }
 
-function getGreeting(user) {
-  if (user) {
-    return <h1>Hello, {formatName(user)}!</h1>
-  }
-  return <h1>Hello, Stranger.</h1>
-}
+// function getGreeting(user) {
+//   if (user) {
+//     return <h1>Hello, {formatName(user)}!</h1>
+//   }
+//   return <h1>Hello, Stranger.</h1>
+// }
 
 // const user = {
 //   firstName: 'Harper',
 //   lastName: 'Perez'
 // }
 
-const user = null
+// const user = null
+
+// const element = (
+//   getGreeting(user)
+// )
 
 const element = (
-  <h1>
-   {getGreeting(user)}
+  <h1 className="greeting">
+    Hello, world!
   </h1>
 )
+
+// const element = React.createElement(
+//   'h1',
+//   {className: 'greeting'},
+//   'Hello, world!'
+// )
 
 ReactDOM.render(
   // <React.StrictMode>
