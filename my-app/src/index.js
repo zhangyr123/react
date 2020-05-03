405da0e94e2be96d9b88b5169d86239ee532fd38
zhangyr <2195175822@qq.com>
2020-05-03 23:06:11 +0800

    元素遍历 条件渲染

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -263,15 +263,65 @@ function GuestGreeting(props) {
 }
 
 function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
+  const isLoggedIn = props.isLoggedIn
   if(isLoggedIn) {
     return <UserGreeting />
   }
   return <GuestGreeting />
 }
 
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Login
+    </button>
+  )
+}
+
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>
+      Logout
+    </button>
+  )
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleLoginClick = this.handleLoginClick.bind(this)
+    this.handleLogoutClick = this.handleLogoutClick.bind(this)
+    this.state = {isLoggedIn: false}
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true})
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false})
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn
+    let button
+    if(isLoggedIn) {
+      button = <LogoutButton onClick={this.handleLogoutClick} />
+    } else {
+      button = <LoginButton onClick={this.handleLoginClick} />
+    }
+
+    return (
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <Greeting isLoggedIn={false} />,
+  <LoginControl />,
   document.getElementById('root')
 )
 
