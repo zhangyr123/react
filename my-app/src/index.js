ba40fc1a4ed70b473de824fd3291b2b7b0ef88d5
zhangyr <2195175822@qq.com>
2020-05-03 22:28:27 +0800

    回调中使用this

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -254,6 +254,35 @@ import * as serviceWorker from './serviceWorker';
 
 // setInterval(tick, 1000)
 
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true}
+
+    // 在回调中使用this
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }))
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON' : 'OFF'}
+      </button>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+)
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
