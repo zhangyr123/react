a68314536cb8e9adcb0c0b95b15e83b48b80f45b
zhangyr <2195175822@qq.com>
2020-05-05 15:12:03 +0800

    textarea 表单

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -393,24 +393,97 @@ import * as serviceWorker from './serviceWorker';
 //   document.getElementById('root')
 // )
 
-function ListItem(props) {
-  return <li>{props.value}</li>
+// function ListItem(props) {
+//   return <li>{props.value}</li>
+// }
+
+// function NumberList(props) {
+//   const numbers = props.numbers
+//   const listItems = numbers.map((number, index) => 
+//     // <li key={index}>{number}</li>
+//     <ListItem key={index} value={number} />
+//   )
+//   return (
+//     <ul>{listItems}</ul>
+//   )
+// }
+
+// const numbers = [1, 2, 3, 4, 5]
+// ReactDOM.render(
+//   <NumberList numbers={numbers} />,
+//   document.getElementById('root')
+// )
+
+// class NameForm extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {value: ''}
+
+//     this.handleChange = this.handleChange.bind(this)
+//     this.handleSubmit = this.handleSubmit.bind(this)
+//   }
+
+//   handleChange(event) {
+//     this.setState({value: event.target.value})
+//   }
+
+//   handleSubmit(event) {
+//     alert('提交的名字： ' + this.state.value)
+//     // event.preventDefault()
+//   }
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           名字：
+//           <input type="text" value={this.state.value} onChange={this.handleChange} />
+//         </label>
+//         <input type="submit" value="提交" />
+//       </form>
+//     )
+//   }
+// }
+
+// ReactDOM.render(
+//   <NameForm />,
+//   document.getElementById('root')
+// )
+
+class EssayForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event) {
+    alert('提交的文章：' + this.state.value)
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          文章:
+          <textarea placeholder="请撰写一篇关于你喜欢的 DOM 元素的文章" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="提交" />
+      </form>
+    )
+  }
 }
 
-function NumberList(props) {
-  const numbers = props.numbers
-  const listItems = numbers.map((number, index) => 
-    // <li key={index}>{number}</li>
-    <ListItem key={index} value={number} />
-  )
-  return (
-    <ul>{listItems}</ul>
-  )
-}
-
-const numbers = [1, 2, 3, 4, 5]
 ReactDOM.render(
-  <NumberList numbers={numbers} />,
+  <EssayForm />,
   document.getElementById('root')
 )
 
