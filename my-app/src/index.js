83496efeac4b135fb4797bf27173d14f9ba5b0f4
zhangyr <2195175822@qq.com>
2020-05-05 15:33:56 +0800

    处理多个输入

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -487,44 +487,87 @@ import * as serviceWorker from './serviceWorker';
 //   document.getElementById('root')
 // )
 
-class FlavorForm extends React.Component {
+// class FlavorForm extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {value: 'coconut'}
+
+//     this.handleChange = this.handleChange.bind(this)
+//     this.handleSubmit = this.handleSubmit.bind(this)
+//   }
+
+//   handleChange(event) {
+//     this.setState({value: event.target.value})
+//   }
+
+//   handleSubmit(event) {
+//     alert('你喜欢的风味是：' + this.state.value)
+//     event.preventDefault()
+//   }
+
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           选择你喜欢的风味：
+//           <select value={this.state.value} onChange={this.handleChange}>
+//             <option value="grapefruit">葡萄柚</option>
+//             <option value="lime">酸橙</option>
+//             <option value="coconut">椰子</option>
+//             <option value="mango">芒果</option>
+//           </select>
+//         </label>
+//         <input type="submit" value="提交" />
+//       </form>
+//     )
+//   }
+// }
+
+// ReactDOM.render(
+//   <FlavorForm />,
+//   document.getElementById('root')
+// )
+
+class Reservation extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {value: 'coconut'}
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    }
 
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
   }
 
-  handleChange(event) {
-    this.setState({value: event.target.value})
-  }
+  handleInputChange(event) {
+    const target = event.target
+    const value = target.name === 'isGoing' ? target.checked : target.value
+    const name = target.name
 
-  handleSubmit(event) {
-    alert('你喜欢的风味是：' + this.state.value)
-    event.preventDefault()
+    this.setState({
+      [name]: value
+    })
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form>
         <label>
-          选择你喜欢的风味：
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="grapefruit">葡萄柚</option>
-            <option value="lime">酸橙</option>
-            <option value="coconut">椰子</option>
-            <option value="mango">芒果</option>
-          </select>
+          参与：
+          <input name="isGoing" type="checkbox" checked={this.state.isGoing} onChange={this.handleInputChange} />
         </label>
-        <input type="submit" value="提交" />
+        <br />
+        <label>
+          来宾人数：
+          <input name="numberOfGuests" type="number" value={this.state.numberOfGuests} onChange={this.handleInputChange} />
+        </label>
       </form>
     )
   }
 }
 
 ReactDOM.render(
-  <FlavorForm />,
+  <Reservation />,
   document.getElementById('root')
 )
 
